f36764ed5354b5668b41bd08ea9978d292e35da9
Garrison McMullen <garrison@ofgskmfx.mynetworksettings.com>
2023-03-19 22:18:16 -0400

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `garments of skin`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
@@ -121,5 +121,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`
   ],
 }
